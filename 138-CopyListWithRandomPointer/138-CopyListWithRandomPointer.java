a3d2a09b2f0befc3993aab0ece4926ba20b39bcf
SAHITYA S <sahirohi123@gmail.com>
2026-07-09 10:09:07 +0530

    Create 138-CopyListWithRandomPointer

---
+++ 138-CopyListWithRandomPointer/138-CopyListWithRandomPointer.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 10:09:06 AM
+public class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        
+        HashMap<Node, Node> oldToNew = new HashMap<>();
+        
+        Node curr = head;
+        while (curr != null) {
+            oldToNew.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+        
+        curr = head;
+        while (curr != null) {
+            oldToNew.get(curr).next = oldToNew.get(curr.next);
+            oldToNew.get(curr).random = oldToNew.get(curr.random);
+            curr = curr.next;
+        }
+        
+        return oldToNew.get(head);
+    }
+}
