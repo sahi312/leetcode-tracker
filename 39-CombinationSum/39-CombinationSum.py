a1152577151031463d5ce658fbf1e465b6cc1d85
SAHITYA S <sahirohi123@gmail.com>
2026-07-09 10:11:10 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.py
@@ -0,0 +1,22 @@
+# Last updated: 7/9/2026, 10:11:05 AM
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+
+        def backtrack(start, path, total):
+            if total == target:
+                result.append(path[:])
+                return
+
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, total + candidates[i])  # reuse same element
+                path.pop()
+
+        backtrack(0, [], 0)
+        return result
