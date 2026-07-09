cf078b87bf8ce1119f5df5e6c9603584738e8a6d
SAHITYA S <sahirohi123@gmail.com>
2026-07-09 10:07:30 +0530

    Create 654-MaximumBinaryTree

---
+++ 654-MaximumBinaryTree/654-MaximumBinaryTree.java
@@ -0,0 +1,37 @@
+// Last updated: 7/9/2026, 10:07:28 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        
+        Stack<TreeNode> stack = new Stack<>();
+        for(int num: nums) {
+
+            TreeNode current = new TreeNode(num);
+            while(!stack.isEmpty() && stack.peek().val < num) {
+                current.left = stack.pop();
+            }
+            if(!stack.isEmpty()) {
+                stack.peek().right = current;
+            }
+            stack.push(current);
+        }
+        while(stack.size() > 1) {
+            stack.pop();
+        }
+        return stack.peek();
+    }
+}
