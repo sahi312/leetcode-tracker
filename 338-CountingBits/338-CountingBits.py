585b1453350dfca5fd4eaf3cc390d9d87e16ae1f
SAHITYA S <sahirohi123@gmail.com>
2026-07-09 10:07:45 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.py
@@ -0,0 +1,13 @@
+# Last updated: 7/9/2026, 10:07:44 AM
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        sub = 1
+
+        for i in range(1, n + 1):
+            if sub * 2 == i:
+                sub = i
+            
+            dp[i] = dp[i - sub] + 1
+        
+        return dp
