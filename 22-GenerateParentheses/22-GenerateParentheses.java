2c2aa6c34c0d315d594c4d7a08ce4e8ae53733a7
SAHITYA S <sahirohi123@gmail.com>
2026-07-09 10:11:28 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -0,0 +1,41 @@
+// Last updated: 7/9/2026, 10:11:27 AM
+import java.util.*;
+
+class Solution {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+
+        backtrack(result, "", 0, 0, n);
+
+        return result;
+    }
+
+    private void backtrack(List<String> result,
+                           String current,
+                           int open,
+                           int close,
+                           int n) {
+
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(result,
+                      current + "(",
+                      open + 1,
+                      close,
+                      n);
+        }
+
+        if (close < open) {
+            backtrack(result,
+                      current + ")",
+                      open,
+                      close + 1,
+                      n);
+        }
+    }
+}
