ce1e1d8c9f88c9e4390d62a5a4c53bbdabbfac10
SAHITYA S <sahirohi123@gmail.com>
2026-07-09 10:06:15 +0530

    Create 4332-CheckGoodInteger

---
+++ 4332-CheckGoodInteger/4332-CheckGoodInteger.c
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 10:06:14 AM
+bool checkGoodInteger(int n) {
+    int digitsum = 0;
+    int squaresum = 0;
+     while (n > 0){
+         int digit = n % 10;
+         digitsum += digit;
+         squaresum += digit * digit;
+         n /= 10;
+     }
+    return (squaresum - digitsum) >= 50;
+}
