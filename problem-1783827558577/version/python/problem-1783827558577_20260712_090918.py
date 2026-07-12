6aa518324ea3e00760f83336468b30e5a2265a23
SAHITYA S <sahirohi123@gmail.com>
2026-07-12 09:09:19 +0530

    Create

---
+++ problem-1783827558577/version/python/problem-1783827558577_20260712_090918.py
@@ -0,0 +1,16 @@
+# Last updated: 7/12/2026, 9:09:18 AM
+1class Solution:
+2    def minimumCost(self, nums,k):
+3        MOD=10**9+7
+4        resource=k
+5        ops=0
+6        ans=0
+7        for x in nums:
+8            if resource < x:
+9                need=(x-resource +k - 1) // k
+10                ans =(ans+(ops+1+ops+need)*need // 2)%MOD
+11                ops += need
+12                resource += need*k
+13            resource -= x
+14        return ans    
+15        
