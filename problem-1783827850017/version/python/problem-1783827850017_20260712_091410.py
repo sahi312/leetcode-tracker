930a798ccdc769236121b9b0d1c46869915e7514
SAHITYA S <sahirohi123@gmail.com>
2026-07-12 09:14:11 +0530

    Create

---
+++ problem-1783827850017/version/python/problem-1783827850017_20260712_091410.py
@@ -0,0 +1,8 @@
+# Last updated: 7/12/2026, 9:14:10 AM
+1class Solution:
+2    def secondsBetweenTimes(self, startTime: str, endTime: str) -> int:
+3        h1,m1,s1 = map(int,startTime.split(":"))
+4        h2,m2,s2=map(int,endTime.split(":"))
+5        start = h1*3600+m1*60+s1
+6        end=h2*3600+m2*60+s2
+7        return end - start
